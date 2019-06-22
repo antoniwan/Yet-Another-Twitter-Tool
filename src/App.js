eab2a9ae1b49d1758c484f77ff3983046e78768e
Antonio Rodriguez <antoniwan@Antonios-MacBook-Pro.local>
2019-06-22 01:00:26 -0400

    Start the basic app layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
+function Connector() {
+  const [connectorIsLoading, setConnectionIsLoading] = useState(false);
+
+  function handleClick() {
+    console.log("click!");
+    setConnectionIsLoading(true);
+  }
+
+  return (
+    <Button
+      variant="primary"
+      onClick={handleClick}
+      disabled={connectorIsLoading}
+    >
+      {connectorIsLoading ? "Loading..." : "Connect with Twitter"}
+    </Button>
+  );
+}
 function App() {
-  return <div>hello World!</div>;
+  return (
+    <div className="app">
+      <Connector />
+    </div>
+  );
 }
 
 export default App;
