afedde72c734fd81297ff6c76e959e23821c98a9
Antonio Rodriguez <antoniwan@gmail.com>
2019-06-28 18:55:45 -0400

    add basic react-router router component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useReducer, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import ValidateUser from "./containers/ValidateUser";
 import { OauthContext } from "./libs/oauth";
 import Connector from "./components/Connector";
 import authReducer from "./reducers/authreducer";
@@ -22,6 +25,11 @@ function App() {
     <div className="app">
       <OauthContext.Provider value={dispatchAuth}>
         {!authenticated && <Connector auth={auth} />}
+
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/oauth/callback/twitter" component={ValidateUser} />
+        </Router>
       </OauthContext.Provider>
     </div>
   );
